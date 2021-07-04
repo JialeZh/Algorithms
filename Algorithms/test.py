b804ddbf438ba2e3970d8faacdb2c6a9ca89de96
Jiale <jiale@gmail.com>
2021-07-04 11:50:22 -0500

    test

--- Algorithms/test.py
+++ Algorithms/test.py
@@ -1,13 +1,1 @@
-List = []
-def fizzBuzz(self, n):
-    for i in range(n):
-         if i % 3 == 0 and i % 5 == 0:
-            List.append("FizzBuzz")
-        elif i % 3 == 0:
-            List.append("Fizz")
-        elif i % 5 == 0:
-            List.append("Buzz")
-        else:
-            List.append(i)
-
-print(fizzBuzz())
+This is testing commmit
